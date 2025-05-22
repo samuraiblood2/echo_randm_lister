81cc444de4617419d48222fa47bae2fac529ff36
samuraiblood2 <someguy2191@aim.com>
2025-05-22 02:14:19 -0400

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-// main.js - This file is deprecated.
-// The main application logic has been moved to RssFeedView.js and App.js
-// for the Single Page Application (SPA) structure.
-// The old main() function is no longer called.
